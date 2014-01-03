cff859f814ac7e6705cac32d5418151c040087ab
Charlie Gaines <beaugaines@gmail.com>
2014-01-03 13:46:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Offsetmybeef::Application.config.secret_key_base = 'ab51e04d6e0ef36895ea4330e2a4a1a9b9e528e6752225440b0b602504050c8b152839111dd3c9a79b5ed42ced38953b64c9b8e7b5b36c5aa5d2e84983b00762'
