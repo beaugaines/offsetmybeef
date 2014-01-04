0e05553e2382e6e168578188ad23c12b79c467ca
Charlie Gaines <beaugaines@gmail.com>
2014-01-03 23:41:47 -0500

    controller test running, pending

---
+++ spec/controllers/transactions_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe TransactionsController do
+  describe 'POST #create' do
+    context 'successful post' do
+      it 'creates a sales object' do
+        pending
+      end
+    end
+  end
+end
