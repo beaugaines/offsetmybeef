e03bef50809196c53e3ab4d50700c632ba75e831
Charlie Gaines <beaugaines@gmail.com>
2014-01-03 22:38:27 -0500

    buy routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Offsetmybeef::Application.routes.draw do
 
   resources :offsets, only: [:index]
 
+  get '/buy/:permalink', to: 'transactions#new', as: :show_buy
+  post '/buy/:permalink', to: 'transactions#create', as: :buy
+
   authenticated :user do 
     root to: 'dashboards#show', as: 'authenticated_root'
   end
