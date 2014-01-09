cd5b875fc24ebc174eb7ae989bbde0b766cb41da
Charlie Gaines <beaugaines@gmail.com>
2014-01-08 22:07:20 -0500

    title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def formatted_price(amount)
     sprintf("$%0.2f", amount / 100.0)
   end
+
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << 'Offset My Beef').join(' - ')
+      end
+    end
+  end
+  
 end
